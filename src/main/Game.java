12e0bd424689b7a89e477edca3cb810e1a148e1a
Calum <calum.rogers@ntlworld.com>
2021-11-23 18:59:14 +0000

    third commit, second page with choice buttons

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,6 +2,8 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game {
 
@@ -9,14 +11,22 @@ public class Game {
     JFrame window;
     Container container;
 //    Create Title Screen, button
-    JPanel titleScreenPanel, startButtonPanel;
+    JPanel titleScreenPanel, startButtonPanel, mainTextPanel, choiceButtonPanel;
 //    Creates the text to display
     JLabel titleScreenLabel;
 //    Creates a custom font called titleFont
-    Font titleFont = new Font("Times New Roman", Font.PLAIN, 80);
-    Font buttonFont= new Font("Times New Roman", Font.PLAIN, 30);
+    Font titleFont = new Font("manaspace", Font.PLAIN, 60);
+    Font buttonFont= new Font("Karmatic Arcade", Font.PLAIN, 30);
+    Font choiceButtonFont = new Font("yoster island",Font.PLAIN, 30);
+    Font textFont = new Font("manaspace", Font.PLAIN,30);
 //    Creates Button
-    JButton startButton;
+    JButton startButton, choice1, choice2, choice3, choice4;
+//Create text area
+    JTextArea mainTextArea;
+
+    TitleScreenHandler titleScreenHandler = new TitleScreenHandler();
+
+
 
 
     public static void main(String[] args){
@@ -62,7 +72,10 @@ public class Game {
         startButton = new JButton("START");
         startButton.setBackground(Color.black);
         startButton.setForeground(Color.white);
+        startButton.setBorder(null);
         startButton.setFont(buttonFont);
+//        function for button- listens for action(when you click start button, button recognises click and calls titleScreenHandler
+        startButton.addActionListener(titleScreenHandler);
 
 
         //        add button to panel
@@ -84,4 +97,79 @@ public class Game {
 //        Makes the window appear to the user
         window.setVisible(true);
     }
+
+
+//Creating Text
+    public void createGameScreen(){
+//when createGameScreen is called titleScreenPanel and startButtonPanel will be "disabled"(hide away)
+     titleScreenPanel.setVisible(false);
+     startButtonPanel.setVisible(false);
+
+
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
+        mainTextPanel.setBackground(Color.black);
+        container.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("Welcome to Codebreaker");
+        mainTextArea.setBounds(100,100,600,250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(textFont);
+
+
+//        Text will wrap automatically without this the text will keep going to the right
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
+
+
+
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(250,350,300,150);
+        choiceButtonPanel.setBackground(Color.black);
+//        this sets a custom grid layout (4: vertical, horizontal:1
+        choiceButtonPanel.setLayout(new GridLayout(4,1));
+        container.add(choiceButtonPanel);
+
+//        choice1 Button
+        choice1 = new JButton("Choice 1");
+        choice1.setBackground(Color.black);
+        choice1.setForeground(Color.white);
+        choice1.setFont(choiceButtonFont);
+        choiceButtonPanel.add(choice1);
+
+//      choice2 Button
+        choice2 = new JButton("Choice 2");
+        choice2.setBackground(Color.black);
+        choice2.setForeground(Color.white);
+        choice2.setFont(choiceButtonFont);
+        choiceButtonPanel.add(choice2);
+//      choice3 button
+        choice3 = new JButton("Choice 3");
+        choice3.setBackground(Color.black);
+        choice3.setForeground(Color.white);
+        choice3.setFont(choiceButtonFont);
+        choiceButtonPanel.add(choice3);
+//      choice4 button
+        choice4 = new JButton("Choice 4");
+        choice4.setBackground(Color.black);
+        choice4.setForeground(Color.white);
+        choice4.setFont(choiceButtonFont);
+        choiceButtonPanel.add(choice4);
+
+
+
+    }
+
+//    button function - the function wil lbe called on when button is clicked
+
+    public class TitleScreenHandler implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
+       createGameScreen();
+
+    }
+}
 }
